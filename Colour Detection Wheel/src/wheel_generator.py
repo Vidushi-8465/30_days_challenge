53b2e4bb83bbcb1ac821932ce2ecc92891e94ccb
Vidushi Bhardwaj <Vidushibhardwaj.217051@gmail.com>
2025-12-25 10:58:26 +0530

    Day 15

--- Colour Detection Wheel/src/wheel_generator.py
+++ Colour Detection Wheel/src/wheel_generator.py
@@ -1,21 +1,22 @@
-import cv2
+import cv2    # 
 import numpy as np
 
-def generate_color_wheel(colors, size=400):
+def generate_colour_wheel(colors, size=400):
     """
-    Draws a circular color wheel using dominant colors
+    Draws a circular color wheel using dominant colours
     """
     wheel = np.zeros((size, size, 3), dtype=np.uint8)
     center = size // 2
     radius = size // 2 - 10
     angle_step = 360 // len(colors)
 
-    for i, color in enumerate(colors):
+    for i, colour in enumerate(colors):
         start_angle = i * angle_step
         end_angle = start_angle + angle_step
 
-        # ✅ FIX: Convert NumPy array to tuple of ints (BGR)
-        bgr_color = tuple(int(c) for c in color[::-1])
+        # 
+        # FIX: Convert NumPy array to tuple of ints (BGR)
+        bgr_colour = tuple(int(c) for c in colour[::-1])
 
         cv2.ellipse(
             wheel,
@@ -24,7 +25,7 @@ def generate_color_wheel(colors, size=400):
             0,
             start_angle,
             end_angle,
-            bgr_color,
+            bgr_colour,
             -1
         )
 
